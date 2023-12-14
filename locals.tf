c1dba75df8a98f743b235b3d3d7cc8a1132952a4
mrmitcp1 <mrmitcp55@gmail.com>
2023-12-14 21:19:32 +0700

    doing-lab

--- locals.tf
+++ locals.tf
@@ -1,17 +1,13 @@
 locals {
   vpc_cidr_block     = var.vpc_cidr_block
-  private_cidr_block = var.private_cidr_block
-  public_cidr_block  = var.public_cidr_block
   instance_type      = var.instance_type
   project_name       = var.project_name
 
   sample_local_is_not_store_in_state_file = "This is a sample local variable"
-  sample_local_02 = aws_instance.private_instance.private_ip
+  subnet_id_for_ec2 = { for k, v in var.subnets : k => aws_subnet.public_net[k].id if v.az == "ap-southeast-1a" }
+
 }
 
 output "sample_output" {
   value = local.sample_local_is_not_store_in_state_file
-}
-output "sample_local_02" {
-    value = local.sample_local_02
 }

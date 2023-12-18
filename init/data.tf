46a326198e7e96f83d37e3f7fb544007d930291f
mrmitcp1 <mrmitcp55@gmail.com>
2023-12-18 21:15:13 +0700

    doing lab with hungran p2
    Signed-off-by: mrmitcp1 <mrmitcp55@gmail.com>

--- init/data.tf
+++ init/data.tf
@@ -13,3 +13,27 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
+data "aws_caller_identity" "current" {}
+# data "aws_ami" "lab" {
+#   depends_on = [ aws_ami_from_instance.ami ]
+#   executable_users = ["self",data.aws_caller_identity.current.account_id]
+#   most_recent      = true
+#   # name_regex       = "^doing-stack-\\d{3}"
+#   owners           = ["self"]
+
+#   filter {
+#     name   = "name"
+#     values = ["doing-stack-*"]
+#   }
+
+#   # filter {
+#   #   name   = "root-device-type"
+#   #   values = ["ebs"]
+#   # }
+
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+# }
+  
--- init/main.tf
+++ init/main.tf
@@ -1,14 +1,13 @@
 module "init" {
   source = "../"
-  user_data_public_instance = templatefile("${path.module}/resources/userData.tftpl", {})
   subnets = var.subnets
 }
 resource "aws_instance" "public_instance" {
   ami                         = data.aws_ami.ubuntu.id
-  instance_type               = var.instance_type
-  subnet_id                   = local.subnet_id_for_ec2
+  instance_type               = var.instance.type
+  subnet_id                   = module.init.public_subnets[var.instance.subnet]
   
-  user_data                   = try(var.user_data_public_instance, null)
+  user_data = templatefile("${path.module}/resources/userData.tftpl", {})
   associate_public_ip_address = true
   tags = {
     Name = "public-instance"
